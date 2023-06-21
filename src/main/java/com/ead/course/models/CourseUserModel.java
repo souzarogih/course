5e23f19ea59f78214e2e171ea8806878e39791ac
HigorSouza <higor.andrade@hotmail.com>
2023-06-21 08:56:08 -0300

    Adicionando campo para pesquisa por userId

--- src/main/java/com/ead/course/models/CourseUserModel.java
+++ src/main/java/com/ead/course/models/CourseUserModel.java
@@ -19,10 +19,9 @@ public class CourseUserModel implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    private CourseModel course;
-
     @Column(nullable = false)
     private UUID userId;
 
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private CourseModel course;
 }
