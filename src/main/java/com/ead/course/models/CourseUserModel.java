62d059438b30fc4378a6377b034d74a049dc112c
HigorSouza <higor.andrade@hotmail.com>
2023-06-24 12:26:00 -0300

    Criando primeira parte do endpoint de inscrição em curso.

--- src/main/java/com/ead/course/models/CourseUserModel.java
+++ src/main/java/com/ead/course/models/CourseUserModel.java
@@ -1,7 +1,9 @@
 package com.ead.course.models;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 import javax.persistence.*;
@@ -9,6 +11,8 @@ import java.io.Serializable;
 import java.util.UUID;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Entity
 @Table(name = "TB_COURSES_USERS")
@@ -19,10 +23,10 @@ public class CourseUserModel implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    private CourseModel course;
-
     @Column(nullable = false)
     private UUID userId;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private CourseModel course;
 
 }
